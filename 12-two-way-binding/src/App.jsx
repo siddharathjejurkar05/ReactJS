12d0796683b68e287bbfcb819e1f8e71c27e0fce
Mac <mac@MacBookPro.lan>
2026-07-19 20:05:43 +0530

    12-two-way-binding

---
+++ 12-two-way-binding/src/App.jsx
@@ -0,0 +1,32 @@
+import { useState } from "react";
+
+const App = () => {
+
+  const [title, settitle] = useState('')
+
+  const submitHandler = (e)=>{
+      e.preventDefault()
+    console.log('Form Submitted by',title);
+
+    settitle('')
+    
+  }
+  return (
+    <div>
+       <form onSubmit={(e)=>{
+         submitHandler(e)
+      }}>
+        <input type="text" 
+        placeholder="Enter Your Name.." 
+        value={title}
+        onChange={(e)=>{
+          settitle(e.target.value);
+        }}
+        />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
