c75722f13590bf7692eaab22e9d952f66c9abf60
Mac <mac@MacBookPro.lan>
2026-07-19 20:01:51 +0530

    11-form-handling

---
+++ 11-form-handling/src/App.jsx
@@ -0,0 +1,20 @@
+const App = () => {
+
+  const submitHandler = (e)=>{
+      e.preventDefault()
+    console.log('Form Submitted');
+    
+  }
+  return (
+    <div>
+      <form onSubmit={(e)=>{
+         submitHandler(e)
+      }}>
+        <input type="text" placeholder="Enter Your Name.." />
+        <button>Submit</button>
+      </form>
+    </div>
+  )
+}
+
+export default App
